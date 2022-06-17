3d7472bee2280fa6fe01a4bb750efa36daa4c735
Zhile Yang <yzlc080733@aliyun.com>
2022-06-17 20:34:22 +0100

    Create linux_post_installation.sh

---
+++ linux_tools/linux_post_installation.sh
@@ -0,0 +1,18 @@
+# Some tools useful
+
+sudo apt update
+
+sudo apt install -y build-essential
+
+sudo apt install -y htop tmux bmon vim nano
+
+sudo apt install -y filezilla putty firefox chromium-browser
+
+sudo apt install -y texlive-full lyx
+
+# ViZDoom (REF: https://github.com/mwydmuch/ViZDoom)
+sudo apt install cmake libboost-all-dev libsdl2-dev libfreetype6-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libjpeg-dev libbz2-dev libfluidsynth-dev libgme-dev libopenal-dev zlib1g-dev timidity tar nasm
+
+# mujoco-py (REF: https://github.com/openai/mujoco-py)
+sudo apt install -y     curl     git     libgl1-mesa-dev     libgl1-mesa-glx     libglew-dev     libosmesa6-dev     software-properties-common     net-tools     vim     virtualenv     wget     xpra     xserver-xorg-dev
+
